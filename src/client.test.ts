1332bd450a207abdcb81e0b2cb4b12bc5ee0ad95
Maximilian Krause <max@kuatsu.dev>
2022-09-18 23:10:22 +0200

    test: added error class tests & extended api tests

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,5 +1,7 @@
 import './tests/setup';
 import { UptimeRobotClient } from './client';
+import { ApiError } from './error';
+import { FetchError } from 'node-fetch';
 
 let uptimeRobotClient: UptimeRobotClient;
 
@@ -13,21 +15,48 @@ test('api key is defined', () => {
   expect(uptimeRobotClient.config.apiKey).not.toEqual('');
 });
 
+test('should throw ApiError when api key is invalid', async () => {
+  const client = new UptimeRobotClient({
+    apiKey: 'invalid',
+  });
+
+  await expect(client.getAccountDetails()).rejects.toThrowError(ApiError);
+});
+
+test('should throw FetchError when calling an invalid base url', async () => {
+  const client = new UptimeRobotClient({
+    apiKey: process.env.UPTIMEROBOT_TEST_API_TOKEN || '',
+    baseUrl: 'https://invalid',
+  });
+
+  await expect(client.getAccountDetails()).rejects.toThrowError(FetchError);
+});
+
 test('get account details', async () => {
   const response = await uptimeRobotClient.getAccountDetails();
 
   expect(response.stat).toBe('ok');
   expect(response.account).toBeDefined();
-  expect(response.account?.monitor_limit).toBeGreaterThan(0);
+  expect(response.account?.email).toBeDefined();
+  expect(response.account?.monitor_limit).toBeDefined();
+  expect(response.account?.monitor_interval).toBeDefined();
+  expect(response.account?.up_monitors).toBeDefined();
+  expect(response.account?.down_monitors).toBeDefined();
+  expect(response.account?.paused_monitors).toBeDefined();
 });
 
 test('get monitors', async () => {
   const response = await uptimeRobotClient.getMonitors();
 
-  console.log(response);
-
   expect(response.stat).toBe('ok');
   expect(response.monitors).toBeDefined();
   expect(response.monitors?.length).toBeGreaterThan(0);
   expect(response.monitors?.[0].id).toBeDefined();
+  expect(response.monitors?.[0].friendly_name).toBeDefined();
+  expect(response.monitors?.[0].url).toBeDefined();
+  expect(response.monitors?.[0].type).toBeDefined();
+  expect(response.monitors?.[0].sub_type).toBeDefined();
+  expect(response.monitors?.[0].keyword_type).toBeDefined();
+  expect(response.monitors?.[0].keyword_case_type).toBeDefined();
+  expect(response.monitors?.[0].keyword_value).toBeDefined();
 });
